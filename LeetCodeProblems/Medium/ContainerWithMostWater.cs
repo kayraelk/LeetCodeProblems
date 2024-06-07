7c49be9d2dc261302a1092850c48e7aa22390a5c
Mustafa Kayra <mustafa.kayra@kion.com.tr>
2024-06-07 10:20:26 +0300

    11. Container With Most Water

---
+++ LeetCodeProblems/Medium/ContainerWithMostWater.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Medium
+{
+    internal class ContainerWithMostWater
+    {
+        public int MaxArea(int[] height)
+        {
+            int left = 0;
+            int right = height.Length - 1;
+            int maxArea = 0;
+
+            while (left < right)
+            {
+                int width = right - left;
+                int currentArea = Math.Min(height[left], height[right]) * width;
+                maxArea = Math.Max(maxArea, currentArea);
+
+                if (height[left] < height[right])
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+
+            return maxArea;
+        }
+    }
+}
--- LeetCodeProblems/Program.cs
+++ LeetCodeProblems/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//11. Container With Most Water
+//https://leetcode.com/problems/container-with-most-water/description/
+
+using LeetCodeProblems.Medium;
+
+ContainerWithMostWater containerWithMostWater = new ContainerWithMostWater();
+int[] height = { 10, 8, 6, 2, 5, 9, 8, 3, 8 };
+Console.WriteLine(containerWithMostWater.MaxArea(height));
+Console.ReadLine();
+
+
