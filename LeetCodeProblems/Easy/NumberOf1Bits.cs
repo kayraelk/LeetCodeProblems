7a3ee628dacc324859e781529ced85032cb363fe
Mustafa Kayra <mustafa.kayra@kion.com.tr>
2024-06-13 01:05:04 +0300

    191. Number of 1 Bits

---
+++ LeetCodeProblems/Easy/NumberOf1Bits.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Easy
+{
+    public class NumberOf1Bits
+    {
+        public int HammingWeight(int n)
+        {
+            int setBits = 0;
+            while (n > 0)
+            {
+                setBits += Convert.ToInt32((n & 1));
+                n >>= 1;
+            }
+            return setBits;
+        }
+    }
+}
--- LeetCodeProblems/Program.cs
+++ LeetCodeProblems/Program.cs
@@ -2,6 +2,7 @@
 //11. Container With Most Water
 //https://leetcode.com/problems/container-with-most-water/description/
 
+using LeetCodeProblems.Easy;
 using LeetCodeProblems.Medium;
 
 //ContainerWithMostWater containerWithMostWater = new ContainerWithMostWater();
@@ -11,23 +12,42 @@ using LeetCodeProblems.Medium;
 
 //15. 3Sum
 //https://leetcode.com/problems/3sum/description/
-Three3Sum three3Sum = new Three3Sum();
+//Three3Sum three3Sum = new Three3Sum();
+//// Example 1
+//int[] nums1 = new int[] { -1, 0, 1, 2, -1, -4 };
+//IList<IList<int>> result1 = three3Sum.ThreeSum(nums1);
+//Console.WriteLine("Example 1:");
+//Three3Sum.PrintResult(result1);
+
+//// Example 2
+//int[] nums2 = new int[] { 0, 1, 1 };
+//IList<IList<int>> result2 = three3Sum.ThreeSum(nums2);
+//Console.WriteLine("Example 2:");
+//Three3Sum.PrintResult(result2);
+
+//// Example 3
+//int[] nums3 = new int[] { 0, 0, 0 };
+//IList<IList<int>> result3 = three3Sum.ThreeSum(nums3);
+//Console.WriteLine("Example 3:");
+//Three3Sum.PrintResult(result3);
+
+
+//191. Number of 1 Bits
+//https://leetcode.com/problems/number-of-1-bits/description/
+NumberOf1Bits numberOf1Bits = new NumberOf1Bits();
+
 // Example 1
-int[] nums1 = new int[] { -1, 0, 1, 2, -1, -4 };
-IList<IList<int>> result1 = three3Sum.ThreeSum(nums1);
-Console.WriteLine("Example 1:");
-Three3Sum.PrintResult(result1);
+int input1 = 11;
+int result1 = numberOf1Bits.HammingWeight(input1);
+Console.WriteLine($"Input: {input1}, Output: {result1}"); // Output: 3
 
 // Example 2
-int[] nums2 = new int[] { 0, 1, 1 };
-IList<IList<int>> result2 = three3Sum.ThreeSum(nums2);
-Console.WriteLine("Example 2:");
-Three3Sum.PrintResult(result2);
+int input2 = 128;
+int result2 = numberOf1Bits.HammingWeight(input2);
+Console.WriteLine($"Input: {input2}, Output: {result2}"); // Output: 1
 
 // Example 3
-int[] nums3 = new int[] { 0, 0, 0 };
-IList<IList<int>> result3 = three3Sum.ThreeSum(nums3);
-Console.WriteLine("Example 3:");
-Three3Sum.PrintResult(result3);
-
+int input3 = 2147483645;
+int result3 = numberOf1Bits.HammingWeight(input3);
+Console.WriteLine($"Input: {input3}, Output: {result3}"); // Output: 30
 
