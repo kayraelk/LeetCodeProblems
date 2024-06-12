c94e29035a5a7fbf57e564d56086560f92be1497
Mustafa Kayra <mustafa.kayra@kion.com.tr>
2024-06-12 08:50:56 +0300

    15. 3Sum

---
+++ LeetCodeProblems/Medium/Three3Sum.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Medium
+{
+    internal class Three3Sum
+    {
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            Array.Sort(nums); // Step 1: Sort the array
+            IList<IList<int>> result = new List<IList<int>>();
+
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                if (i > 0 && nums[i] == nums[i - 1])
+                {
+                    continue; // Skip duplicate elements
+                }
+
+                int left = i + 1, right = nums.Length - 1;
+
+                while (left < right)
+                {
+                    int total = nums[i] + nums[left] + nums[right];
+
+                    if (total == 0) // If we find a triplet
+                    {
+                        result.Add(new List<int> { nums[i], nums[left], nums[right] });
+                        while (left < right && nums[left] == nums[left + 1]) left++; // Skip duplicates
+                        while (left < right && nums[right] == nums[right - 1]) right--; // Skip duplicates
+                        left++;
+                        right--;
+                    }
+                    else if (total < 0)
+                    {
+                        left++; // Move left pointer to the right to increase the sum
+                    }
+                    else
+                    {
+                        right--; // Move right pointer to the left to decrease the sum
+                    }
+                }
+            }
+
+            return result;
+        }
+        internal static void PrintResult(IList<IList<int>> result)
+        {
+            foreach (var triplet in result)
+            {
+                Console.WriteLine("[" + string.Join(", ", triplet) + "]");
+            }
+            if (result.Count == 0)
+            {
+                Console.WriteLine("[]");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
--- LeetCodeProblems/Program.cs
+++ LeetCodeProblems/Program.cs
@@ -4,9 +4,30 @@
 
 using LeetCodeProblems.Medium;
 
-ContainerWithMostWater containerWithMostWater = new ContainerWithMostWater();
-int[] height = { 10, 8, 6, 2, 5, 9, 8, 3, 8 };
-Console.WriteLine(containerWithMostWater.MaxArea(height));
-Console.ReadLine();
+//ContainerWithMostWater containerWithMostWater = new ContainerWithMostWater();
+//int[] height = { 10, 8, 6, 2, 5, 9, 8, 3, 8 };
+//Console.WriteLine(containerWithMostWater.MaxArea(height));
+//Console.ReadLine();
+
+//15. 3Sum
+//https://leetcode.com/problems/3sum/description/
+Three3Sum three3Sum = new Three3Sum();
+// Example 1
+int[] nums1 = new int[] { -1, 0, 1, 2, -1, -4 };
+IList<IList<int>> result1 = three3Sum.ThreeSum(nums1);
+Console.WriteLine("Example 1:");
+Three3Sum.PrintResult(result1);
+
+// Example 2
+int[] nums2 = new int[] { 0, 1, 1 };
+IList<IList<int>> result2 = three3Sum.ThreeSum(nums2);
+Console.WriteLine("Example 2:");
+Three3Sum.PrintResult(result2);
+
+// Example 3
+int[] nums3 = new int[] { 0, 0, 0 };
+IList<IList<int>> result3 = three3Sum.ThreeSum(nums3);
+Console.WriteLine("Example 3:");
+Three3Sum.PrintResult(result3);
 
 
