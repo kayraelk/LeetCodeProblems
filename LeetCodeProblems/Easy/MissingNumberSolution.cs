7f09c1e95b08d981e7251f4a9d8328a1fb7448be
Mustafa Kayra <mustafa.kayra@kion.com.tr>
2024-06-14 17:25:26 +0300

    268. Missing Number

---
+++ LeetCodeProblems/Easy/MissingNumberSolution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Easy
+{
+    public class MissingNumberSolution
+    {
+        public int MissingNumber(int[] nums)
+        {
+            // Step 1: Sort the array
+            Array.Sort(nums);
+
+            // Step 2: Find the first position where the index does not match the value
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != i)
+                {
+                    return i;
+                }
+            }
+
+            // If all indices match, then the missing number is n
+            return nums.Length;
+        }
+    }
+}
--- LeetCodeProblems/Program.cs
+++ LeetCodeProblems/Program.cs
@@ -1,16 +1,16 @@
-﻿
-//11. Container With Most Water
-//https://leetcode.com/problems/container-with-most-water/description/
-
-using LeetCodeProblems.Easy;
+﻿using LeetCodeProblems.Easy;
 using LeetCodeProblems.Medium;
+
+#region 11. Container With Most Water
+//https://leetcode.com/problems/container-with-most-water/description/
 
 //ContainerWithMostWater containerWithMostWater = new ContainerWithMostWater();
 //int[] height = { 10, 8, 6, 2, 5, 9, 8, 3, 8 };
 //Console.WriteLine(containerWithMostWater.MaxArea(height));
 //Console.ReadLine();
+#endregion
 
-//15. 3Sum
+#region 15. 3Sum
 //https://leetcode.com/problems/3sum/description/
 //Three3Sum three3Sum = new Three3Sum();
 //// Example 1
@@ -30,9 +30,9 @@ using LeetCodeProblems.Medium;
 //IList<IList<int>> result3 = three3Sum.ThreeSum(nums3);
 //Console.WriteLine("Example 3:");
 //Three3Sum.PrintResult(result3);
+#endregion
 
-
-//191. Number of 1 Bits
+#region Number of 1 Bits
 //https://leetcode.com/problems/number-of-1-bits/description/
 //NumberOf1Bits numberOf1Bits = new NumberOf1Bits();
 
@@ -50,9 +50,30 @@ using LeetCodeProblems.Medium;
 //int input3 = 2147483645;
 //int result3 = numberOf1Bits.HammingWeight(input3);
 //Console.WriteLine($"Input: {input3}, Output: {result3}"); // Output: 30
+#endregion
 
-//338. Counting Bits
+#region 338. Counting Bits
 //https://leetcode.com/problems/counting-bits/description/
-CountingBits countingBits = new CountingBits();
-int[] result = countingBits.CountBits(5);
-Console.WriteLine(string.Join(",", result)); // Output: 0,1,1,2,1,2
+//CountingBits countingBits = new CountingBits();
+//int[] result = countingBits.CountBits(5);
+//Console.WriteLine(string.Join(",", result)); // Output: 0,1,1,2,1,2
+#endregion
+
+#region 268. Missing Number
+//https://leetcode.com/problems/missing-number/description/
+MissingNumberSolution missingNumberSolution = new MissingNumberSolution();
+
+// Example 1
+int[] nums1 = { 3, 0, 1 };
+Console.WriteLine("Missing number in [3, 0, 1] is: " + missingNumberSolution.MissingNumber(nums1)); // Output: 2
+
+// Example 2
+int[] nums2 = { 0, 1 };
+Console.WriteLine("Missing number in [0, 1] is: " + missingNumberSolution.MissingNumber(nums2)); // Output: 2
+
+// Example 3
+int[] nums3 = { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
+Console.WriteLine("Missing number in [9, 6, 4, 2, 3, 5, 7, 0, 1] is: " + missingNumberSolution.MissingNumber(nums3)); // Output: 8
+
+#endregion
+
